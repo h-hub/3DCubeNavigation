761a46d632e51aed9dc0fdf9843dc11c1c7fd98b
h-hub <harsha.kj89@gmail.com>
2017-05-23 11:42:31 -0700

    add files

--- cubejs/main.js
+++ cubejs/main.js
@@ -33,7 +33,7 @@
 
 				    //camera
 				    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-				    camera.position.z = 300;
+				    camera.position.z = 350;
 
 				    //lights
 				    directionalLight = new THREE.DirectionalLight(0xeff3ea, 1.2);
@@ -76,20 +76,56 @@
 						})
 				    	]);
 
-				    var geometryBtn = new THREE.BoxGeometry( 8, 10, 30 );				
+				    var cubeMaterialBtn3 = new THREE.MeshFaceMaterial([
+				    	new THREE.MeshBasicMaterial({
+					        color: 0xFF0000
+					    }),
+				    	new THREE.MeshBasicMaterial({
+					        color: 0xFF0000
+					    }),
+				    	new THREE.MeshBasicMaterial({
+					        color: 0xFF0000
+					    }),
+				    	new THREE.MeshBasicMaterial({
+					        map: THREE.ImageUtils.loadTexture( 'image/youtube.jpg' )
+					    }),
+				    	new THREE.MeshBasicMaterial({
+					        color: 0xFF0000
+					    }),
+				    	new THREE.MeshBasicMaterial({
+							color: 0xFF0000
+						})
+				    	]);
+
+				    var geometryBtn = new THREE.BoxGeometry( 4, 10, 30 );				
 				    Btn = new THREE.Mesh( geometryBtn, cubeMaterialBtn );
 				    Btn.name = "btn";
-				    Btn.position.set (74,-20,2)
+				    Btn.position.set (74,-10,2)
 
-				    var geometryBtn2 = new THREE.BoxGeometry( 8, 10, 30 );
+				    var geometryBtn2 = new THREE.BoxGeometry( 4, 10, 30 );
 				    Btn2 = new THREE.Mesh( geometryBtn, cubeMaterialBtn );
 				    Btn2.name = "btn2";
 				    Btn2.position.set (-74,-20,2)
 
-				    var geometryBtn3 = new THREE.BoxGeometry( 30, 5, 10 );
-				    Btn3 = new THREE.Mesh( geometryBtn3, cubeMaterialBtn );
+				    var geometryBtn3 = new THREE.BoxGeometry( 30, 4, 20 );
+				    Btn3 = new THREE.Mesh( geometryBtn3, cubeMaterialBtn3 );
 				    Btn3.name = "btn3";
 				    Btn3.position.set (0,-75,-35)
+
+				    var geometryBtn4 = new THREE.BoxGeometry( 30, 4, 10 );
+				    Btn4 = new THREE.Mesh( geometryBtn4, cubeMaterialBtn );
+				    Btn4.name = "btn4";
+				    Btn4.position.set (0,75,5)
+
+				    var geometryBtn5 = new THREE.BoxGeometry( 30, 10, 4 );				
+				    Btn5 = new THREE.Mesh( geometryBtn5, cubeMaterialBtn );
+				    Btn5.name = "btn5";
+				    Btn5.position.set (0,-20,75)
+
+				    var geometryBtn6 = new THREE.BoxGeometry( 30, 10, 4 );				
+				    Btn6 = new THREE.Mesh( geometryBtn6, cubeMaterialBtn );
+				    Btn6.name = "btn6";
+				    Btn6.position.set (0,-40,-75)
 
 
 				    var cubeMaterial = new THREE.MeshFaceMaterial([
@@ -99,7 +135,7 @@
 					    }),
 				    	new THREE.MeshBasicMaterial({
 					        // color: 0x5e980f
-					        map: THREE.ImageUtils.loadTexture( 'image/neth3d.png' )
+					        map: THREE.ImageUtils.loadTexture( 'image/cameradevice.png' )
 					    }),
 				    	new THREE.MeshBasicMaterial({
 					        // color: 0x5e980f
@@ -111,10 +147,10 @@
 					    }),
 				    	new THREE.MeshBasicMaterial({
 					        // color: 0x5e980f
-					        map: THREE.ImageUtils.loadTexture( 'image/cameradevice.png' )
+					        map: THREE.ImageUtils.loadTexture( 'image/neth3d.png' )
 					    }),
 				    	new THREE.MeshBasicMaterial({
-				    		map: THREE.ImageUtils.loadTexture( 'image/neth3d.png' )
+				    		map: THREE.ImageUtils.loadTexture( 'image/neth3d2.png' )
 				    	})
 				    	]);
 				    var geometry = new THREE.BoxGeometry( 150,150,150 );
@@ -130,7 +166,14 @@
 
 				    cube.add(Btn2);
 
+				    //youtube button
 				    cube.add(Btn3);
+
+				    cube.add(Btn4);
+
+				    cube.add(Btn5);
+
+				    cube.add(Btn6);
 
 				    scene.add(cube);
 
@@ -138,11 +181,14 @@
 
 				    //we add the even listener function to the domElement
 				    renderer.domElement.addEventListener( 'mousedown', onMouseDown );
-				    renderer.setClearColor (0xeff3ea, 1);
+				    renderer.setClearColor (0xeff3ea, 1);window.addEventListener( 'resize', onWindowResize, false );
 
-				    // console.log(cube.rotation.x +"-"+cube.rotation.y+"-"+cube.rotation.z);
-				    
-				    //cube.rotation.y = Math.PI / 2;
+				    window.addEventListener( 'resize', onWindowResize, false );
+				    $('#viewPort').on( 'touchstart', onDocumentTouchStart );
+					$('#viewPort').on( 'touchmove', onDocumentTouchMove );
+
+					cube.rotation.x = 0.4;
+					cube.rotation.y = 0.7;
 				}
 
 				function animate() {
@@ -152,6 +198,15 @@
 
 				}
 
+				function onWindowResize() {
+					WIDTH = $('#viewPort').innerWidth()
+					HEIGHT = $('#viewPort').innerHeight();
+					ASPECT = WIDTH / HEIGHT;
+					camera.aspect = ASPECT;
+					camera.updateProjectionMatrix();
+					renderer.setSize(WIDTH, HEIGHT);
+				}
+
 				//event listener
 				function onMouseDown( event_info ) {
 				    //stop any other event listener from recieving this event
@@ -159,13 +214,8 @@
 
 				    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 				    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-				    
-				    console.log($('#viewPort').offset().top+ "-"+HEIGHT);
-				    //this where begin to transform the mouse cordinates to three,js cordinates
-				    mouse.x = ( event_info.clientX / WIDTH ) * 2 - 1;
-				    mouse.y = - ( event_info.clientY / (HEIGHT + $('#viewPort').offset().top)) * 2 + 1 ;
-
-                    mouse.x  = ((event_info.clientX - $('#viewPort').position().left) / $('#viewPort').width()) * 2 - 1;
+				   
+                    mouse.x  = ( event_info.clientX / WIDTH ) * 2 - 1;
                     mouse.y = -((event_info.clientY - $('#viewPort').position().top) / $('#viewPort').height()) * 2 + 1;
 
 				    mouseX = event.clientX;
@@ -195,7 +245,7 @@
                     //the ray will return an array with length of 1 or greater if the mouse click
                     //does touch the sphere object
                     if( (vector.z > 0) && (intersectsBtn.length ) ) {
-                            console.log( " perfect sphere hit btn1" );
+                        window.open("http://neth3d.com/");
                     }
 
                     intersectsBtn = ray.intersectObject(Btn2);
@@ -207,7 +257,7 @@
                     intersectsBtn = ray.intersectObject(Btn3);
                     vector.setFromMatrixPosition( Btn3.matrixWorld );
                     if( (vector.z > 0) && (intersectsBtn.length ) ) {
-                        console.log( " perfect sphere hit btn3" );
+                        $('#exampleModal').modal('show');
                     }
 
 				}
@@ -234,33 +284,98 @@
 
 				}
 
+				function onDocumentTouchStart( event ) {
+					if ( event.touches.length === 1 ) {
+						event.preventDefault();
+						// mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+						// targetRotationOnMouseDown = targetRotation;
+
+						mouse.x  = ( event.touches[ 0 ].pageX / WIDTH ) * 2 - 1;
+	                    mouse.y = -((event.touches[ 0 ].pageY - $('#viewPort').position().top) / $('#viewPort').height()) * 2 + 1;
+
+					    mouseX = event.touches[ 0 ].pageX;
+					    mouseY = event.touches[ 0 ].pageY;
+					    
+					    //this vector caries the mouse click cordinates
+					    mouse_vector.set( mouse.x, mouse.y, mouse.z );
+					    
+					    //the final step of the transformation process, basically this method call
+					    //creates a point in 3d space where the mouse click occurd
+					    projector.unprojectVector( mouse_vector, camera );
+					    
+					    var direction = mouse_vector.sub( camera.position ).normalize();
+					    
+					    //ray = new THREE.Raycaster( camera.position, direction );
+					    ray.set( camera.position, direction );
+					    
+					    //asking the raycaster if the mouse click touched the sphere object
+					    intersects = ray.intersectObject(cube);
+					    intersectsBtn = ray.intersectObject(Btn);
+
+					    //-----------------
+					    cube.updateMatrixWorld();
+					    var vector = new THREE.Vector3();
+					    vector.setFromMatrixPosition( Btn.matrixWorld );
+
+	                    //the ray will return an array with length of 1 or greater if the mouse click
+	                    //does touch the sphere object
+	                    if( (vector.z > 0) && (intersectsBtn.length ) ) {
+	                            console.log( " perfect sphere hit btn1" );
+	                    }
+
+	                    intersectsBtn = ray.intersectObject(Btn2);
+	                    vector.setFromMatrixPosition( Btn2.matrixWorld );
+	                    if( (vector.z > 0) && (intersectsBtn.length ) ) {
+	                        console.log( " perfect sphere hit btn2" );
+	                    }
+
+	                    intersectsBtn = ray.intersectObject(Btn3);
+	                    vector.setFromMatrixPosition( Btn3.matrixWorld );
+	                    if( (vector.z > 0) && (intersectsBtn.length ) ) {
+	                        $('#exampleModal').modal('show');
+                    }
+					}
+				}
+
+				function onDocumentTouchMove( event ) {
+					if ( event.touches.length === 1 ) {
+						event.preventDefault();
+
+						var deltaX = event.touches[ 0 ].pageX - mouseX,
+						deltaY = event.touches[ 0 ].pageY - mouseY;
+						mouseX = event.touches[ 0 ].pageX;
+						mouseY = event.touches[ 0 ].pageY;
+						rotateScene(deltaX, deltaY);
+					}
+				}
+
 				function rotateScene(deltaX, deltaY) {
 					cube.rotation.y += deltaX / 100;
 					cube.rotation.x += deltaY / 100;
 				}
 
-				function showVideo(){
-					cube.rotation.x = - Math.PI / 2;
-					cube.rotation.y =  0;
+				function showSide(x,y){
+					cube.rotation.x = x;
+					cube.rotation.y = y;
 				}
 
 				init();
 				animate();
 
 				$(document).ready(function(){
-			    /* Get iframe src attribute value i.e. YouTube video url
-			    and store it in a variable */
-			    var url = $("#video").attr('src')+"?autoplay=1";
-			    
-			    /* Assign empty url value to the iframe src attribute when
-			    modal hide, which stop the video playing */
-			    $("#exampleModal").on('hide.bs.modal', function(){
-			    	$("#video").attr('src', '');
-			    });
-			    
-			    /* Assign the initially stored url back to the iframe src
-			    attribute when modal is displayed again */
-			    $("#exampleModal").on('show.bs.modal', function(){
-			    	$("#video").attr('src', url);
-			    });
-			});
+				    /* Get iframe src attribute value i.e. YouTube video url
+				    and store it in a variable */
+				    var url = $("#video").attr('src')+"?autoplay=1";
+				    
+				    /* Assign empty url value to the iframe src attribute when
+				    modal hide, which stop the video playing */
+				    $("#exampleModal").on('hide.bs.modal', function(){
+				    	$("#video").attr('src', '');
+				    });
+				    
+				    /* Assign the initially stored url back to the iframe src
+				    attribute when modal is displayed again */
+				    $("#exampleModal").on('show.bs.modal', function(){
+				    	$("#video").attr('src', url);
+				    });
+				});
